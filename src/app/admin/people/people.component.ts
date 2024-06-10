8c598964e5e13dc90a558ba45b7f7fbd6d16ca8c
Daniel Bispo <daniel.bispo@datacoress.com>
2024-06-10 17:19:56 -0300

    Layout preparado para açõe

--- src/app/admin/people/people.component.ts
+++ src/app/admin/people/people.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
 import { PeopleService } from '../../services/people.service';
 import { Person } from '../../models/person.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './people.component.html',
   styleUrl: './people.component.scss'
 })
 export class PeopleComponent {
+  people: Person[] = [];
+  newName: string = '';
 
-  constructor(private service: PeopleService){
+  constructor(private service: PeopleService) {
     this.get();
   }
 
-  get(){
-    this.service.getAll().subscribe((peoples: Person[])=>{
-      console.log(peoples);
+  get() {
+    this.service.getAll().subscribe((people: Person[]) => {
+      this.people = people;
     });
+  }
+
+  save(person: Person) {
+    console.log(person);
+  }
+
+  saveNew(){
+    console.log(this.newName);
   }
 }
