9ade35b70c25ca35c6ca49a67aae9beb06f5c671
Daniel Bispo <daniel.bispo@datacoress.com>
2024-06-12 14:14:57 -0300

    FEAT: criado tabuleiro do jogo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { PeopleComponent } from './admin/people/people.component';
 import { QuestionsComponent } from './admin/questions/questions.component';
+import { BoardComponent } from './game/board/board.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: BoardComponent
+    },
     {
         path: 'admin/people',
         component: PeopleComponent
