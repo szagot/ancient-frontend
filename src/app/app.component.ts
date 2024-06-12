41085faa8cf9b92495332c964b7d4fddccf16cbd
Daniel Bispo <daniel.bispo@datacoress.com>
2024-06-12 14:06:22 -0300

    FEAT: DRY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ancient-frontend';
+  navItems = [
+    { route: '/', icon: 'bi-joystick', label: 'Jogo' },
+    { route: '/admin/people', icon: 'bi-people-fill', label: 'Pessoas' },
+    { route: '/admin/questions', icon: 'bi-patch-question-fill', label: 'Perguntas' },
+  ];
 
-  constructor(private routes: Router){}
+  constructor(private routes: Router) { }
 
-  go(url: string){
+  go(url: string) {
     this.routes.navigateByUrl(url);
     return false;
   }
